4e0af0ec6623ad0059f7925ecae5aa27936f7353
begimov <begimov@gmail.com>
2017-11-05 10:46:51 +0300

    mocking and setting up frequencies trait moved to a separate method

--- tests/Unit/FrequenciesTest.php
+++ tests/Unit/FrequenciesTest.php
@@ -10,10 +10,16 @@ class FrequenciesTest extends TestCase
     /** @test **/
     public function canSetPlainCronExpression()
     {
-        $frequencies =  $this->getMockForTrait(FrequenciesTrait::class);
-        $frequencies->expression = '* * * * *';
+        $frequencies = $this->setUpFrequencies();
         $frequencies->cron('23 12 * * 1');
 
         $this->assertEquals($frequencies->expression, '23 12 * * 1');
+    }
+
+    protected function setUpFrequencies()
+    {
+        $frequencies =  $this->getMockForTrait(FrequenciesTrait::class);
+        $frequencies->expression = '* * * * *';
+        return $frequencies;
     }
 }
